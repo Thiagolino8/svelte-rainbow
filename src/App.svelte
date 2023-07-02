78277093e25bc2a165259056116304d7387aef44
Thiago <thiagolinog8@gmail.com>
2023-07-02 11:11:18 -0300

    compact

--- src/App.svelte
+++ src/App.svelte
@@ -1,21 +1,15 @@
 <script lang="ts">
 	import rainbowGradient from 'rainbow-gradient'
-	import { onMount } from 'svelte'
 
-	const rainbowColors = rainbowGradient(360).map(([r, g, b]: number[]) => `rgb(${r},${g},${b})`)
+	const rainbowColors = rainbowGradient(360).map(([r, g, b]) => `rgb(${r},${g},${b})`)
 	let colors = new Array(360 * 4).fill(0).map((_, i) => rainbowColors[i % rainbowColors.length])
+	const length = colors.length;
 
-	const update = () => {
-		colors.push(colors.shift() as string)
+	(function update() {
+		colors.push(colors.shift())
 		colors = colors
 		requestAnimationFrame(update)
-	}
-
-	onMount(() => {
-		requestAnimationFrame(update)
-	})
-
-	const length = colors.length
+	})()
 </script>
 
 <main
@@ -25,6 +19,6 @@
     width: 100vw;"
 >
 	{#each colors as color}
-		<div style="background-color: {color};" />
+			<div style="background-color: {color};" />
 	{/each}
 </main>
